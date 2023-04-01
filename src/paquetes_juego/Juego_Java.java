318bd6f968483ce77c8e4ceeb7e42bc9da5f3c2c
Oscar Garcia Hernandez <oscargh0901@gmail.com>
2023-04-01 14:08:48 +0200

    Cambio estetico UI 2

--- src/paquetes_juego/Juego_Java.java
+++ src/paquetes_juego/Juego_Java.java
@@ -1,50 +1,69 @@
 package paquetes_juego;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Juego_Java {
-
-    public static void main(String[] args) {
-        menu();
+public class Juego_Java extends JFrame implements ActionListener {
+    
+    public Juego_Java() {
+        super("Juego");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 1));
+        
+        JButton nuevaPartidaButton = new JButton("Nueva partida");
+        nuevaPartidaButton.addActionListener(this);
+        panel.add(nuevaPartidaButton);
+        
+        JButton cargarPartidaButton = new JButton("Cargar partida");
+        cargarPartidaButton.addActionListener(this);
+        panel.add(cargarPartidaButton);
+        
+        JButton ajustesButton = new JButton("Ajustes");
+        ajustesButton.addActionListener(this);
+        panel.add(ajustesButton);
+        
+        JButton salirButton = new JButton("Salir");
+        salirButton.addActionListener(this);
+        panel.add(salirButton);
+        
+        add(panel);
+        setVisible(true);
     }
     
-    public static void menu() {
-        boolean menu = false;
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
         
-        do {
-            int respuesta = JOptionPane.showOptionDialog(null, "MENU PRINCIPAL", "Juego", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new Object[] {"Nueva partida", "Cargar partida", "Ajustes", "Salir"}, null);
-            
-            switch(respuesta) {
+        if (action.equals("Nueva partida")) {
+            int nueva_partida = JOptionPane.showOptionDialog(this, "¿Desea comenzar una nueva aventura?", "Juego", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new Object[] {"Comenzar", "Cancelar"}, null);
+                    
+            switch(nueva_partida) {
                 case 0:
-                    int nueva_partida = JOptionPane.showOptionDialog(null, "¿Desea comenzar una nueva aventura?", "Juego", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new Object[] {"Comenzar", "Cancelar"}, null);
-                    
-                    switch(nueva_partida) {
-                        case 0:
-                            menu = false;
-                            logica();
-                            break;
-                            
-                        case 1:
-                            menu = true;
-                            break;
-                    }
-                    
+                    logica();
                     break;
                     
                 case 1:
-                    //obtener datos de la partida
-                    break;
-                    
-                case 2:
-                    //obtener los ajustes del usuario
-                    break;
-                    
-                case 3:
-                    JOptionPane.showMessageDialog(null, "Saliendo...");
-                    System.exit(0);
                     break;
             }
-        } while(menu);
+        }
+        else if (action.equals("Cargar partida")) {
+            //obtener datos de la partida
+        }
+        else if (action.equals("Ajustes")) {
+            //obtener los ajustes del usuario
+        }
+        else if (action.equals("Salir")) {
+            JOptionPane.showMessageDialog(this, "Saliendo...");
+            System.exit(0);
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Juego_Java();
     }
     
     public static void logica() {
